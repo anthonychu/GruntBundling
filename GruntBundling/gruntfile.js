eac6c2bba0ff3d2dc4bd8bc5577d65239edc1b13
Anthony Chu <anthony@anthonychu.ca>
2015-11-23 00:48:22 -0800

    Minor updates to grunt file

--- GruntBundling/gruntfile.js
+++ GruntBundling/gruntfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding AfterBuild='build' ProjectOpened='watch:bundles' />
+﻿/// <binding BeforeBuild='build' Clean='clean' ProjectOpened='watch:bundles' />
 /*
 This file in the main entry point for defining grunt tasks and using grunt plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
@@ -65,7 +65,7 @@ module.exports = function (grunt) {
         
         watch: {
             bundles: {
-                files: ['Scripts/**/*.js', 'Content/**/*.js'],
+                files: ['Scripts/**/*.js', 'Content/**/*.css'],
                 tasks: ['build']
             }
         }
